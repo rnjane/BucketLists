aed58d791006a1d60c4d970803420c392736da38
Robert Ndungu <robert.ndungu@hotmail.com>
2017-07-19 14:20:32 +0300

    [CHORE]Rename tests to show edge cases

--- test_all.py
+++ test_all.py
@@ -8,7 +8,7 @@ class ViewTest(unittest.TestCase):
     def setUp(self):
         self.app = app.app.test_client()
 
-    def test_index_page(self):
+    def test_indexpage_succesful(self):
         '''Test home page route'''
         response = self.app.get('/', follow_redirects=True)
         print(response.data)
@@ -16,7 +16,7 @@ class ViewTest(unittest.TestCase):
 
 class BucketListTest(unittest.TestCase):
     '''Tests for the Bucket List Class'''
-    def test_add_bucket_list_works(self):
+    def test_addbucketlist_succesful(self):
         '''Tests if add functionality works'''
         user = models.User()
         user.createUser('peter', 1234)
@@ -27,7 +27,7 @@ class BucketListTest(unittest.TestCase):
             checkitem = True
         self.assertEqual(checkitem, True)
 
-    def test_no_duplicate_bucket_lists(self):
+    def test_bucketlists_duplicatebucketlists(self):
         '''Test to check if duplicate bucket list can be added'''
         bucket_list = models.BucketLists()
         bucket_list.create_bucket('Hike')
@@ -38,7 +38,7 @@ class BucketListTest(unittest.TestCase):
             checkexists = True
         self.assertEqual(checkexists, True)
 
-    def test_delete_bucket_list_item(self):
+    def test_deletebucketlist_sucessful(self):
         '''Tests if delete functionality works'''
         bucket_list = models.BucketLists()
         bucket_list.create_bucket('Camp')
@@ -48,7 +48,7 @@ class BucketListTest(unittest.TestCase):
             delete_not_working = True
         self.assertEqual(delete_not_working, False)
 
-    def test_edit_bucket_list_item(self):
+    def test_editbucketlist_succesful(self):
         '''Tests if edit functionality works'''
         bucket_list = models.BucketLists()
         bucket_list.create_bucket('Camp')
@@ -62,7 +62,7 @@ class BucketListTest(unittest.TestCase):
 class BucketListItemTest(unittest.TestCase):
     '''Tests for the Bucket List Items Class'''
 
-    def test_add_bucket_list_item_works(self):
+    def test_additem_succesful(self):
         '''Tests if add functionality works'''
         bkt = models.BucketLists()
         bkt.current_bucket('bucket1')
@@ -73,7 +73,7 @@ class BucketListItemTest(unittest.TestCase):
             add_item_works = True
         self.assertEqual(add_item_works, True)
 
-    def test_no_duplicate_list_item(self):
+    def test_items_duplicateitems(self):
         '''Test to check if duplicate list item can be added'''
         item = models.Items()
         item.create_item('swim')
@@ -84,7 +84,7 @@ class BucketListItemTest(unittest.TestCase):
             duplicate_exists = False
         self.assertEqual(duplicate_exists, False)
 
-    def test_delete_item(self):
+    def test_deleteitem_succesful(self):
         '''Tests if delete functionality works'''
         item = models.Items()
         item.create_item('do assignment')
@@ -95,7 +95,7 @@ class BucketListItemTest(unittest.TestCase):
             delete_not_working = True
         self.assertEqual(delete_not_working, False)
 
-    def test_edit_item(self):
+    def test_edititem_succesful(self):
         '''Tests if edit functionality works'''
         item = models.Items()
         item.create_item('task')
@@ -108,7 +108,7 @@ class BucketListItemTest(unittest.TestCase):
 
 class UserTest(unittest.TestCase):
     '''Tests for user functionalities'''
-    def test_register_works(self):
+    def test_register_succesful(self):
         '''Tests if register functionality works'''
         user = models.User()
         user.createUser('peter', 'password')
@@ -116,7 +116,7 @@ class UserTest(unittest.TestCase):
             checkuser = True
         self.assertEqual(checkuser, True)
 
-    def test_no_duplicate_user(self):
+    def test_register_duplicateusers(self):
         '''Test to check if duplicate username can be added'''
         user = models.User()
         user.createUser('ken', 'pass')
@@ -125,7 +125,7 @@ class UserTest(unittest.TestCase):
             checkexists = True
         self.assertEqual(checkexists, True)
 
-    def test_user_exists(self):
+    def test_login_wrongcredentials(self):
         '''Test user exists before login'''
         user = models.User()
         unameexists = False
@@ -133,7 +133,7 @@ class UserTest(unittest.TestCase):
             unameexists = True
         self.assertEqual(unameexists, True)
 
-    def test_wrong_password_format(self):
+    def test_register_wrongcredentials(self):
         '''check if wrong password is entered'''
         user = models.User()
         check = user.createUser([], 'jj')
